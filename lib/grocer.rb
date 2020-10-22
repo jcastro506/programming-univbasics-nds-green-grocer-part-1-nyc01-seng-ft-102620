26d430ebf00f69d4375075a89c0fbc09f5e7a57d
Josh Castro <castrojoshua805@gmail.com>
2020-10-22 17:04:55 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item, info|
     if collection.include?(name)
-      return collection[:item]
+      p collection[:item]
     end
   end
   NIL
